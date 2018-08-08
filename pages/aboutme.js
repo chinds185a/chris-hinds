6f5cd6dd924eb5cb2a3c25fa67cc35df7ecd30f1
Chris Hinds <chris@chriss-imac.home>
2018-08-08 17:22:42 +0100

    Add timeline component to about me page

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -1,5 +1,7 @@
 import Layout from "../components/layout";
 import styled from "styled-components";
+import Icons from "react-fontawesome";
+import {Timeline, TimelineBlip} from 'react-event-timeline';
 
 // Components
 import Hero from "../components/hero";
@@ -22,7 +24,28 @@ export default () => (
           <article>
             <div className="uk-card uk-card-hover uk-card-body uk-card-large">
               <h3 className="uk-card-title">Timeline</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <Timeline>
+                <TimelineBlip
+                  title="Software Engineer @ British Broadcasting Corporation (BBC)"
+                  icon={<Icons name="book"/>}
+                />
+                <TimelineBlip
+                  title="Freelance Web designer/Developer @ Pixel-FX"
+                  icon={<Icons name="object-group"/>}
+                />
+                <TimelineBlip
+                  title="Advanced Networks &amp; Security @ Bournemouth University"
+                  icon={<Icons name="graduation-cap"/>}
+                />
+                <TimelineBlip
+                  title="IT Enginner &amp; Windows Deployment Technician @ National Oceanography Centre"
+                  icon={<Icons name="laptop"/>}
+                />
+                <TimelineBlip
+                  title="Computing @ Bournemouth University"
+                  icon={<Icons name="graduation-cap"/>}
+                />
+              </Timeline>
             </div>
           </article>
 
